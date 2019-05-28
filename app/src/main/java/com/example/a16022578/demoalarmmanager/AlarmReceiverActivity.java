a6e174169bc867a87702d48e1e1b535d0e3c0372
acidjive <16022578@myrp.edu.sg>
2019-05-28 11:42:31 +0800

    Done

--- app/src/main/java/com/example/a16022578/demoalarmmanager/AlarmReceiverActivity.java
+++ app/src/main/java/com/example/a16022578/demoalarmmanager/AlarmReceiverActivity.java
@@ -1,5 +1,14 @@
 package com.example.a16022578.demoalarmmanager;
 
-public class AlarmReceiverActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
 }
